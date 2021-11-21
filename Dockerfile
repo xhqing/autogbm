680cb478e0a23edbdb772879b6d4a883fbc1a538
xuhuaqing <huaqingxu89@gmail.com>
2021-11-21 19:30:53 +0800

    up

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM registry.cn-shanghai.aliyuncs.com/tcc-public/python:3
+
+RUN mkdir /home/autogbm
+# ADD requirements.txt /
+# RUN pip3 install -r requirements.txt
--- my_pylint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-pylint --rcfile=./.pylintrc autogbm | grep unused-import
-pylint --rcfile=./.pylintrc autogbm | grep missing-function-docstring
-pylint --rcfile=./.pylintrc autogbm | grep unused-variable
-pylint --rcfile=./.pylintrc autogbm | grep import-outside-toplevel
-pylint --rcfile=./.pylintrc autogbm | grep no-else-return
